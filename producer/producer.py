d8789ce4fefe933cc499abaa35cdf01f48153f33
Adriano Gigliuto <adriano.gigliuto@moveax.it>
2023-04-05 20:08:10 +0200

    update producer

--- producer/producer.py
+++ producer/producer.py
@@ -1,6 +1,32 @@
-from confluent_kafka import Producer
+import random
+from collections import namedtuple
 
-p = Producer({'bootstrap.servers': 'localhost:9093'})
+from kafka import KafkaProducer
 
-p.produce(topic='T1', value='some test message', key='KEY', partition=1)
-p.flush()
+# TODO write a self contained script/test which produces a lot of messages
+
+Msg = namedtuple('Msg', 'topic value key partition')
+
+
+def generate_records(topic, num=1000, num_partitions=100):
+    for i in range(num):
+        payload = f'Message: {i}'
+        yield Msg(topic=topic,
+                  value=bytes(payload, encoding='utf-8'),
+                  key=b'Event',
+                  partition=random.randrange(start=1, stop=num_partitions))
+    # yield [Msg(topic=topic,
+    #            value=f'Message: {i}',
+    #            key='Event',
+    #            partition=random.randrange(start=1, stop=num_partitions))
+    #        for i in range(num)]
+
+
+producer = KafkaProducer(bootstrap_servers='localhost:9093')
+for t1_msg, t2_msg in zip(generate_records('T1'), generate_records('T2')):
+    producer.send(topic=t1_msg.topic, value=t1_msg.value, key=t1_msg.key, partition=t1_msg.partition)
+    producer.send(topic=t2_msg.topic, value=t2_msg.value, key=t2_msg.key, partition=t2_msg.partition)
+
+    # producer.flush()
+
+producer.flush()
